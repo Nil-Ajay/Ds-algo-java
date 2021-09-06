a618058958cb99573d204d57ef6c399e76dece11
Nil-Ajay <nileshaher141@gmail.com>
2021-09-06 10:05:24 +0530

    donw!

--- 02_conditionals and loops/Basic java programs/24_largest number among all entered numbers -y/largeNum.java
+++ 02_conditionals and loops/Basic java programs/24_largest number among all entered numbers -y/largeNum.java
@@ -6,12 +6,14 @@ public class largeNum{
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a number : ");
         float num = in.nextFloat();
-		float temp = num;
+        float temp = num;
         while(num != 0){
-			temp = num;
-            System.out.print("Enter a number or enter 0 to exit : ");
+            System.out.print("Enter a number or press 0 to exit : ");
             num = in.nextFloat();
+            if(temp < num){
+                temp = num;
+            };
         };
-        System.out.println(temp);
+        System.out.println("Largest number among numbers you entered is : " + temp);
     };
 };
